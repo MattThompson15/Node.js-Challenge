88d5a6590c92ef73c4b01abbb9393616f56502e3
Matt Thompson <matthewthompson118@gmail.com>
2023-12-10 15:40:26 -0400

    began prompt questions

--- index.js
+++ index.js
@@ -2,7 +2,23 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input';
+        name: 'projectTitle',
+        message: :'Enter your project title';
+    },
+    {
+        type: 'input';
+        name: 'description';
+        message: 'Enter a project description';
+    },
+    {
+        
+    }
+    
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
