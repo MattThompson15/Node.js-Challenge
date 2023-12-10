ec9b51d8e79139f92d806157debf7f49ca91b924
Matt Thompson <matthewthompson118@gmail.com>
2023-12-10 17:00:28 -0400

    added more questions to array

--- index.js
+++ index.js
@@ -4,22 +4,54 @@ const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [
     {
-        type: 'input';
+        type: 'input',
         name: 'projectTitle',
-        message: :'Enter your project title';
+        message:'Enter your project title',
     },
     {
-        type: 'input';
-        name: 'description';
-        message: 'Enter a project description';
+        type: 'input',
+        name: 'description',
+        message: 'Enter a project description',
     },
     {
-        
-    }
-    
-    
-];
+        type: 'input',
+        name: 'installation',
+        message: 'Enter installation instructions',
 
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage information',
+    },
+    {
+        type: 'input',
+        name: 'contributions',
+        message: 'Enter contribution guidelines',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Enter test instructions',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license',
+        choices: ['MIT', 'GPL', 'Apache'],
+    },
+    {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'Enter your Github username',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email adress',
+    },
+];
+    
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
